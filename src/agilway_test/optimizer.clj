800e277874709dd02a63ac3cb4a16ddf764ca269
Kostiantyn Semerheiev <kostyantin@srgh.tech>
2020-10-25 11:15:25 +0200

    Pausing advanced optimization for simple one

--- src/agilway_test/optimizer.clj
+++ src/agilway_test/optimizer.clj
@@ -6,8 +6,28 @@
 (defn split-variables-values [args]
   [(filter variable? args) (filter number? args)])
 
+(defn opposite-value [value]
+  (cond
+    (and (sequential? value) (= (count value) 2))
+    (second value)
+    (number? value)
+    (- value)
+    :else
+    `(- ~value)))
+
+(defn unnest-expressions-in-multiplication [args]
+  (reduce
+    (fn [acc arg]
+      (if (and (sequential? arg) (= (first arg) (symbol :*)))
+        (concat acc (rest arg))
+        (conj acc arg)))
+    []
+    args))
+
 (defn multiplication-optimizer [args]
-  (let [[variables values] (split-variables-values args)
+  (let [[variables values] (-> args
+                               unnest-expressions-in-multiplication
+                               split-variables-values)
         values-mult (reduce * values)]
     (cond
       (zero? values-mult)
@@ -15,18 +35,51 @@
       (empty? variables)
       values-mult
       (= 1 values-mult)
-      variables
+      (if (> (count variables) 1)
+        variables
+        (first variables))
       :else
       (conj variables values-mult))))
 
-(defn division-optimizer [args]
-  (let [base (first args)]
-    (if (and (number? base) (zero? base))
-      0
-      (conj (multiplication-optimizer (rest args)) base))))
+(defn unnest-expressions-in-addition [args]
+  (reduce
+    (fn [acc arg]
+      (if (sequential? arg)
+        (let [func      (first arg)
+              arguments (rest arg)]
+          (case func
+            + (concat acc arguments)
+            - (-> acc
+                  (conj (first arguments))
+                  (concat (map (fn [var]
+                                 (opposite-value var))
+                               (rest arguments))))
+            (conj (vec acc) arg)))
+        (conj acc arg)))
+    []
+    args))
+
+
+(defn even-out-variables-in-addition [[variables values]]
+  (let [grouped    (frequencies variables)
+        evened-out (reduce (fn [acc [key value]]
+                             (let [negated-key (opposite-value key)]
+                               (if-let [negated-val (get acc negated-key)]
+                                 (let [delta (- value negated-val)]
+                                   (if (pos? delta)
+                                     (-> acc (assoc key delta) (dissoc negated-key))
+                                     (-> acc (assoc negated-key (- delta)))))
+                                 (assoc acc key value))))
+                           {}
+                           grouped)]
+    [(reduce (fn [acc [arg times]] (concat acc (repeat times arg))) [] evened-out) values]))
+
 
 (defn addition-optimizer [args]
-  (let [[variables values] (split-variables-values args)
+  (let [[variables values] (-> args
+                               unnest-expressions-in-addition
+                               split-variables-values
+                               even-out-variables-in-addition)
         values-sum (reduce + values)]
     (cond
       (empty? variables)
@@ -36,11 +89,43 @@
         variables
         (first variables))
       :else
-      (conj variables values-sum)))
-  )
+      (conj variables values-sum))))
+
+(defn division-optimizer [args]
+  (let [base                (first args)
+        division-multiplier (multiplication-optimizer (rest args))]
+    (cond
+      (number? base)
+      (cond
+        (zero? base)
+        0
+        (number? division-multiplier)
+        (/ base division-multiplier)
+        (sequential? division-multiplier)
+        (if (number? (first division-multiplier))
+          (conj (rest division-multiplier) (/ base (first division-multiplier)))
+          (conj division-multiplier base))
+        :else
+        (list base division-multiplier))
+      (sequential? base)
+      (list base division-multiplier)
+      (sequential? division-multiplier)
+      (list base division-multiplier)
+      :else
+      (list base division-multiplier))))
 
 (defn subtraction-optimizer [args]
-  (conj (addition-optimizer (rest args)) (first args)))
+  (let [base            (first args)
+        sum-of-negation (addition-optimizer (rest args))]
+    (if (number? base)
+      (cond
+        (number? sum-of-negation)
+        (- base sum-of-negation)
+        (and (sequential? sum-of-negation) (number? (first sum-of-negation)))
+        (conj (rest sum-of-negation) (- base (first sum-of-negation)))
+        :else
+        (flatten [base sum-of-negation]))
+      (flatten [base sum-of-negation]))))
 
 (defn optimize [expression]
   (if (sequential? expression)
@@ -53,20 +138,29 @@
                            - subtraction-optimizer
                            identity)
           optimized-expr (optimize-func args)]
-      (println (optimize-func args))
       (if (sequential? optimized-expr)
         (conj optimized-expr func)
         optimized-expr))
     expression))
 
-(optimize '(* 1 2 3 x y 4 (+ x y)))
+(agilway-test.evaluator/evaluate {:x 1 :y 1} (optimize '(+ x y 1 2 3 (- y 10) (- 10 y))))
 
-(optimize '(* x y))
+;(optimize '(/ x y 2 3 5))
 
-(optimize '(+ 1 2 3 4 (* 1 2 3) x))
+;(optimize '(/ 20 2 z y 2 v))
 
-(optimize '(* 2 3))
+;(optimize '(/ (+ (* y (* z 2)) (- b c 5) 5) (+ y z)))
 
-(optimize '(/ y 2 2 x))
+;(optimize '(+ x (- 1 x)))
 
-(optimize '(- y 3 5 1 x 123))
+(optimize '(/ x (* x y)))
+
+(optimize '(/ (* x y) x y))
+
+(optimize '(/ (* x y) 5))
+
+(optimize '(/ x (+ x y)))
+
+(optimize '(/ 5 y))
+
+;(agilway-test.evaluator/evaluate {:y 2 :z 1 :b 1 :c 1} (optimize '(/ (+ (* y (* z 2)) (- b c 5) 5) (+ y z))))
--- test/agilway_test/optimizer_test.clj
+++ test/agilway_test/optimizer_test.clj
@@ -2,7 +2,52 @@
   (:require [clojure.test :as t])
   (:require [agilway-test.optimizer :as o]))
 
-(t/deftest simple-optimization
-  (t/is (= (o/optimize '(* x 0)) 0)))
+(o/optimize '(* 1 2 3 x y 4 (+ x y)))
+
+(o/optimize '(* x y))
+
+(o/optimize '(+ 1 2 3 4 (* 1 2 3) x))
+
+(o/optimize '(* 2 3))
+
+;(o/optimize '(- y 3 5 1 (- x 123)))
+
+(t/deftest multiplication-optimization
+  (t/is (= (o/optimize '(* (+ x 1 3 2 4) 0)) 0) "Multiple of ANY and zero is zero")
+  (t/is (= (o/optimize '(* 1 1 1 1 x)) 'x) "Multiplying by zero element is pointless")
+  (t/is (= (o/optimize '(* 3 2 1 x y)) '(* 6 x y)) "We calculate values and keep variables")
+  (t/is (= (o/optimize '(* 3 (* 2 1 x (* 5 y)))) '(* 30 x y)) "Should open nested multiplication")
+  (t/is (= (o/optimize '(* (* x y) 5)) '(* 5 x y)) "Should put values in first argument place"))
+
+(t/deftest addition-optimization
+  (t/is (= (o/optimize '(+ 5 3 2)) 10) "Should sum values")
+  (t/is (= (o/optimize '(+ x y 0 5 3 (+ 3 4 z))) '(+ 15 x y z)) "Addition unnesting")
+  (t/is (= (o/optimize '(+ x y 1 2 3 (- y 10) (- 10 y))) '(+ 6 x y )) "Subtraction unnesting and evening out")
+  (t/is (= (o/optimize '(+ x 1 2 3 (* 2 y) (/ 6 2 ) (/ y 4))) '(+ 9 x (* 2 y) (/ y 4)))
+        "Addition should keep order of variables")
+  (t/is (= (o/optimize '(+ (+ x y) 1 2 3)) '(+ 6 x y))
+        "Will arrange values always before variables"))
+
+(t/deftest division-optimization
+  (t/is (= (o/optimize '(/ 0 (+ 3 y) (* 5 z))) 0) "Division is always zero if first element is zero")
+  (t/is (= (o/optimize '(/ x y 2 3 5)) '(/ x 30 y)) "Division is first element / multiple of rest")
+  (t/is (= (o/optimize '(/ 20 2 z y 2 v)) '(/ 5 z y v)) "Division should try to calculate available number"))
+
+(t/deftest subtraction-optimization
+  (t/is (= (o/optimize '(- 5 2 1)) 2) "Should subtract values")
+  (t/is (= (o/optimize '(- 5 (+ 2 1) (/ x 6) (* y 2))) '(- 2 (/ x 6) (* 2 y)))
+        "Should properly calculate values and preserve order of arguments"))
+
+(t/deftest general-optimization-test
+  (let [use-all-expressions '(/ (+ 1 (* 5 3) (- 5 2)) (+ 0 (* 5 (- 3 3 0)) (+ 3 2) (- 5 6) 1))
+        use-all-expressions-variables '(/ (+ (y (z * 2)) (- b c 5) 5) (+ y z))]
+    (t/is (= (o/optimize use-all-expressions) 19/5))))
+
+(o/optimize '(/ (+ (* y (* z 2)) (- b c 5) 5) (+ y z)))
+
+
 
 (t/run-all-tests #"agilway-test.optimizer-test")
+
+
+(o/optimize '(+ x y 1 2 3 (- y 10) (- 10 y)))
